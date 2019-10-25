28e679d545258b6552252755de143363bc99333e
brian91292 <brian@yur.fit>
2019-10-25 00:49:21 -0500

    Bump version

--- EnhancedStreamChat/Plugin.cs
+++ EnhancedStreamChat/Plugin.cs
@@ -22,7 +22,7 @@ namespace EnhancedStreamChat
     {
         public static readonly string ModuleName = "Enhanced Stream Chat";
         public string Name => ModuleName;
-        public string Version => "2.1.0";
+        public string Version => "2.1.1";
         
         public static Plugin Instance { get; private set; }
 
